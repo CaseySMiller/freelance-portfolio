a312052d06cc58202ffaa817d3c21f72312cf5fb
CaseySMiller <CaseySMiller@yahoo.com>
2023-11-27 08:08:18 -0700

    connected flowbite-react

--- src/app/components/Navbar/page.tsx
+++ src/app/components/Navbar/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image'
+import { Button } from 'flowbite-react';
 import logo from '../../../../public/assets/images/logo/Full_Logo_Light_Small.png'
 
 import './style.css'
@@ -14,8 +15,8 @@ export default function CustomNav() {
           width={100}
           height={100}
           priority
-          // className='h-auto'
         />
+
       </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
+import { Button } from 'flowbite-react';
 
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
       <Link href="/testroute">testroute link</Link>
+      <Button>
+        Click Me
+      </Button>
     </>
   )
 }
