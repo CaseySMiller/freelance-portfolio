4a0111e1d7af727003506c1b641b7633c7deb63c
CaseySMiller <CaseySMiller@yahoo.com>
2023-12-15 11:48:07 -0700

    setup layout for home page with OpacityCards and grid with mobile reponsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { Button, Card } from "flowbite-react";
 
 import ThreeJsCarousel from "./components/ThreeJsCarousel/ThreeJsCarousel";
+import OpacityCard from "./components/OpacityCard/OpacityCard";
 
 export default function Home() {
   const imgArr = [
@@ -23,23 +24,28 @@ export default function Home() {
       <div className="carousel-container">
         <ThreeJsCarousel imgArr={imgArr} />
       </div>
-      <div className="container columns-3 mx-auto pt-3 w-100">
-        <div className="w-full h-full">
-          <Card className="w-full relative opacity-25">
-            <p className="opacity-100">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, illo corrupti doloremque voluptatibus, adipisci quisquam nisi nam hic deleniti repellat, debitis nulla dolorum voluptate quidem autem. Velit ab explicabo aliquam.
-            </p>
-          </Card>
+      <div className="container grid lg:grid-cols-5 mx-auto pt-3 w-100 gap-y-4 gap-x-2 px-2 pb-8">
+        <div className="w-full  grid justify-center lg:col-span-5 lg:pt-16">
+          <h1 className="z-10">Home</h1>
         </div>
-        <div className="w-full h-full">
-          <h1 >Home</h1>
+        <div className="w-full lg:h-full lg:col-span-2">
+          <OpacityCard
+            className="self-center"
+            title="Hi There"
+            textContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, illo corrupti doloremque voluptatibus, adipisci quisquam nisi nam hic deleniti repellat, debitis nulla dolorum voluptate quidem autem. Velit ab explicabo aliquam."
+            bgOpacity={0.25}
+            textOpacity={0.9}
+          />
         </div>
-        <div className="w-full h-full">
-          <Card className="w-full relative">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, illo corrupti doloremque voluptatibus, adipisci quisquam nisi nam hic deleniti repellat, debitis nulla dolorum voluptate quidem autem. Velit ab explicabo aliquam.
-            </p>
-          </Card>
+        <div> </div>
+
+        <div className="w-full lg:h-full lg:col-span-2">
+          <OpacityCard
+            title="Hi There #2"
+            textContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, illo corrupti doloremque voluptatibus, adipisci quisquam nisi nam hic deleniti repellat, debitis nulla dolorum voluptate quidem autem. Velit ab explicabo aliquam."
+            bgOpacity={0.25}
+            textOpacity={0.9}
+          />
         </div>
       </div>
     </>
--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -1,3 +1,3 @@
 export const metadata = {
-  manifest: '/manifest.webmanifest',
+  // manifest: '/manifest.webmanifest',
 }
