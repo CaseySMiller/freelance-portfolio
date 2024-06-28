5027b4dc82841c01014575e7a1f525be4ed6f273
CaseySMiller <CaseySMiller@yahoo.com>
2024-06-27 20:41:59 -0600

    added image functionality to OpacityCard

--- src/app/components/OpacityCard/OpacityCard.tsx
+++ src/app/components/OpacityCard/OpacityCard.tsx
@@ -2,7 +2,14 @@
 'use client'; 
 
 import { useRef } from "react";
-import { text } from "stream/consumers";
+import Image from "next/image";
+
+// union type to match up with the requirements of the Image component from nextJS
+type cardImgProps = (
+  {URL: string, alt: string, height: number, width: number, fill?: undefined | false } | 
+  {URL: string, alt: string, height: number, fill: true, width?: undefined } |
+  {URL: string, alt: string, width: number, fill: true, height?: undefined }  
+)
 
 interface tCardProps {
   title?: string;
@@ -14,6 +21,7 @@ interface tCardProps {
   titleClass?: string;
   bgClass?: string;
   className?: string;
+  cardImg?: cardImgProps;
 }
 
 function OpacityCard({
@@ -26,6 +34,7 @@ function OpacityCard({
   titleClass,
   textClass,
   bgClass,
+  cardImg,
 }: tCardProps) {
 
 
@@ -60,9 +69,6 @@ function OpacityCard({
     bgEl.current ? bgEl.current.style.opacity = bgOpacityStr : null; 
     textEl.current ? textEl.current.style.opacity = textOpacityStr : null;
   }
-  // const handleExit = (e) => {e.target.style.opacity = .5}
-  // const handleEnter = (e) => {console.log(bgBox.current.style.opacity)}
-  
 
   return (
     <div
@@ -71,6 +77,7 @@ function OpacityCard({
       onMouseOver={fullOonHover ? handleEnter : undefined}
       onMouseOut={fullOonHover ? handleExit : undefined}
     >
+      {/* this is the background element, the content is hidden and only used to adjust the size */}
       <div
         className={`relative h-fit w-full rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800  ${bgClassTrue}`}
         style={cardStyle.bgStyle}
@@ -81,18 +88,76 @@ function OpacityCard({
           className={`invisible relative flex-grow justify-center p-6 ${textClassTrue}`}
           style={cardStyle.textStyle}
         >
+          {cardImg ? (
+            <div className="pb-2 mb-2">
+              {(cardImg?.height && cardImg?.width) ? (
+                <Image 
+                src={cardImg.URL}
+                alt="positioning placeholder" 
+                width={cardImg.width} 
+                height={cardImg.height} 
+                />
+              ) : null}
+              {(cardImg?.height && cardImg?.fill) ? (
+                <Image 
+                src={cardImg.URL}
+                alt="positioning placeholder" 
+                height={cardImg.height} 
+                fill={cardImg.fill} 
+                />
+              ) : null}
+              {(cardImg?.width && cardImg?.fill) ? (
+                <Image 
+                src={cardImg.URL}
+                alt="positioning placeholder" 
+                width={cardImg.width} 
+                fill={cardImg.fill}
+                />
+              ) : null}
+            </div>
+          ) : null}
+
           {title ? <h2 className={`overflow-clip border-b pb-2 ${titleClassTrue}`}>{title}</h2> : null}
           <p className={`relative border overflow-clip pt-3 ${textClassTrue}`}>
             {textContent}
           </p>
         </div>
       </div>
+      {/* this element contains the visible content */}
       <div className="w-full absolute">
         <div
           className={`relative flex-grow justify-center p-6 ${textClassTrue}`}
           style={cardStyle.textStyle}
           ref={textEl}
         >
+          {cardImg ? (
+            <div className="pb-2 mb-2">
+              {(cardImg?.height && cardImg?.width) ? (
+                <Image 
+                src={cardImg.URL}
+                alt="positioning placeholder" 
+                width={cardImg.width} 
+                height={cardImg.height} 
+                />
+              ) : null}
+              {(cardImg?.height && cardImg?.fill) ? (
+                <Image 
+                src={cardImg.URL}
+                alt="positioning placeholder" 
+                height={cardImg.height} 
+                fill={cardImg.fill} 
+                />
+              ) : null}
+              {(cardImg?.width && cardImg?.fill) ? (
+                <Image 
+                src={cardImg.URL}
+                alt="positioning placeholder" 
+                width={cardImg.width} 
+                fill={cardImg.fill}
+                />
+              ) : null}
+            </div>
+          ) : null}
           {title ? (
             <h2 className={`overflow-clip border-b pb-2 ${titleClassTrue}`}>{title}</h2>
           ) : null}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,24 @@
 import UnderConstruction from "../components/UnderConstruction/UnderConstruction"
+import OpacityCard from "../components/OpacityCard/OpacityCard"
+import Image from "next/image"
 
 export default function Projects() {
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto my-auto w-1/2">
             {/* <h1>Projects</h1> */}
-            <UnderConstruction />
+
+            <OpacityCard 
+            className="w-1/2"
+            title="Project 1"
+            textContent="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem illum blanditiis assumenda impedit nisi necessitatibus quaerat commodi laborum in laudantium."
+            imgOpacity={1}
+            cardImg={{
+                URL: "/assets/images/logo/Full_Logo_Light_Small.png",
+                height: 480,
+                width: 640,
+                alt: "placeholder"
+            }}
+            />
         </div>
     )
 }
