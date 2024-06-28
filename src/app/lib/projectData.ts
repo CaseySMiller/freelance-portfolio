f7b17965f7f6b448b4a9436b107ddd92fa22173f
CaseySMiller <CaseySMiller@yahoo.com>
2024-06-27 22:15:54 -0600

    added projectData

---
+++ src/app/lib/projectData.ts
@@ -0,0 +1,15 @@
+export const projectData = [
+  {
+    id: "1", 
+    rank: 1,
+    title: "2peex Winter Sports",
+    description: "Welcome to the ultimate demo site for an imaginary winter sports company! This cutting-edge JamStack application harnesses the power of NextJS to deliver a sleek, responsive front-end experience. It seamlessly integrates with a demo-mode Shopify store, which functions as a headless CMS. With all data stored on Shopify's secure servers, customers can effortlessly update products, categories, and even pages directly from the Shopify Dashboard—no coding required. Explore the future of e-commerce with this innovative and dynamic website!",
+    images: [
+      "/assets/images/screenshots/jamStack/demo-jamstack-ss.png",
+      "/assets/images/screenshots/jamStack/shopify-ss.png",
+    ],
+    deployedURL: "https://next-shopify-demo-ashen.vercel.app/",
+    repoURL: "https://github.com/CaseySMiller/next-shopify-demo",
+    category: "JamStack",
+  }
+]
